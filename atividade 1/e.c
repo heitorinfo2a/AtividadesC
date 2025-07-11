52c4ebf2eb6acc4cfcd5bbde8c535e965a0bb338
Heitor Alves Vieira da Silva <h3itor4lves@gmail.com>
2025-07-11 09:28:29 -0300

    Add files via upload

---
+++ atividade 1/e.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float f;
+    
+    printf("Digite a temperatura em Fahrenheit \n");
+    scanf("%f",&f);
+    
+    float c = ((f-32)*5) /9;
+    
+    printf("A temperatura em Celsuis e: %.2f\n",c);
+    
+    return 0;
+}
