25c17e3dccec58afb39f96531b22d555479610c9
Heitor Alves Vieira da Silva <h3itor4lves@gmail.com>
2025-07-23 10:14:35 -0300

    Create atividade14.c

---
+++ atividade14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    printf("\t Total \t= 100%\n\t IVA \t= 17%\n\t IRS \t= 15%\n----------------------------- \n\t Liq. \t= 68%\n");
+    return 0;
+}
+
+int main2()
+{
+    printf("");
+}
