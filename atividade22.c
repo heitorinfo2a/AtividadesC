89b253d7c66a274913cd07ea061fc901f699e1ad
Heitor Alves Vieira da Silva <h3itor4lves@gmail.com>
2025-07-23 10:19:02 -0300

    Create atividade22.c

---
+++ atividade22.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a, m, d;
+
+    printf("Informe o ano: ");
+    scanf("%d", &a);
+
+    printf("Informe o mes: ");
+    scanf("%d", &m);
+
+    printf("Informe o dia: ");
+    scanf("%d", &d);
+
+    if(d > 31 || m > 12 || a < 1900){
+        printf("Data inválida, insira novamente.\n");
+    }
+
+    printf("-------------------------------\n");
+
+    printf("Data antes da conversão: %04d/%02d/%02d\n", a, m, d);
+    printf("Data depois da conversão: %02d/%02d/%04d\n", d, m, a);
+
+    return 0;
+}
