ae676f834d7a632d551b0014752edf421fa3a29a
Heitor Alves Vieira da Silva <h3itor4lves@gmail.com>
2025-08-14 11:17:06 -0300

    Update questao5.c

--- atividades_semana3/questao5.c
+++ atividades_semana3/questao5.c
@@ -5,10 +5,14 @@ int main() {
     printf("Digite um numero inteiro: ");
     scanf("%d", &valor);
 
-    if ((valor % 2 == 0 && valor % 3 != 0) || (valor % 2 != 0 && valor % 3 == 0)) {
-        printf("O numero e divisivel por 2 ou por 3, mas nao pelos dois.\n");
+    if (valor % 2 == 0 && valor % 3 == 0) {
+        printf("O numero e divisivel por 2 e por 3.\n");
+    } else if (valor % 2 == 0) {
+        printf("O numero e divisivel por 2, mas nao por 3.\n");
+    } else if (valor % 3 == 0) {
+        printf("O numero e divisivel por 3, mas nao por 2.\n");
     } else {
-        printf("O numero nao atende a condicao.\n");
+        printf("O numero nao e divisivel nem por 2 nem por 3.\n");
     }
 
     return 0;
