70f710cb38633b2270ca8d2020608c8207f708e9
Heitor Alves Vieira da Silva <h3itor4lves@gmail.com>
2025-08-15 09:43:35 -0300

    Update questao2.c

--- atividades_semana3/questao2.c
+++ atividades_semana3/questao2.c
@@ -4,7 +4,7 @@ int main() {
     int x;
     printf("Numero\tQuadrado\tCubo\n");
     for(x = 0; x <= 10; x++) {
-        printf("%d\t%d\t\t%d\n", x, x*x, x*x*x);
+        printf("%-7d%-12d%-7d\n", x, x*x, x*x*x);  // Ajustando o espaço
     }
     return 0;
 }
